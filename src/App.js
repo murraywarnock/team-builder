c9174d5e8340ce1faea90a9fc69ccbdedc57bb7d
MurrayWarnock <murray.warnock@gmail.com>
2021-04-20 12:45:30 -0600

    Initial React app, stripped with just and h1 tag

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+
+      </header>
+      <div>
+        <h1>Team Builder</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
