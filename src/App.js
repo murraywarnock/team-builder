a00d83bdf8e054c67995817279655330f9239e5e
MurrayWarnock <murray.warnock@gmail.com>
2021-04-20 15:47:08 -0600

    MVP working - having a hard time invoking standard css style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,73 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import Form from './Form'
+import Member from './Member'
+
+const starterTeam = [{
+  name: "Captain Wonderful",
+  email: "cap@wonder.com",
+  role: "cowboy",
+}];
+
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  name: '',
+  email: '',
+  ///// DROPDOWN /////
+  role: '',
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header>
+  const [team, setTeam] = useState(starterTeam);
+  const [formValues, setFormValues] = useState(initialFormValues)
 
-      </header>
-      <div>
-        <h1>Team Builder</h1>
-      </div>
+  const updateForm = (inputName, inputValue) => {
+   setFormValues({...formValues, [inputName]: inputValue })
+  }
+
+  const submitForm = () => {
+    // new member object with values from form
+    console.log("submitForm called")
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    // form validation
+    console.log("made newMember", newMember);
+    if (!newMember.name || !newMember.email || !newMember.role) return
+    // add member to team array
+    setTeam([...team, newMember])
+    console.log("setTeam executed:", team);
+    // clear the form
+    setFormValues(initialFormValues)
+    
+  }
+
+  // useEffect(() => {
+  //   setTeam([{
+  //     name: "Captain Wonderful",
+  //     email: "cap@wonder.com",
+  //     role: "cowboy",
+  // }]);
+  // }, [])
+
+  return (
+    <div className="container">
+      <h1>Team Builder</h1>
+      <Form
+        // props for Form
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+
+      {
+        team.map(member => {
+          return (
+            <Member key={member.id} details={member} />
+          )
+        })
+      }
     </div>
   );
 }
